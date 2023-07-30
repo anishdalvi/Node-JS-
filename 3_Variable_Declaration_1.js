c568d45b80d7ff15b63999a60aa26a4e7208e5bb
anishdalvi <anish.201914101@vcet.edu.in>
2023-07-30 12:01:12 +0530

    var declaration 1

--- 3_Variable_Declaration_1.js
+++ 3_Variable_Declaration_1.js
@@ -1,13 +1,13 @@
 function someDec(){
 
-    var num = 10;
+    var num = 50;
 
     function someOther(){
         num  = num + num;
         console.log("Inner Some Other ", num);
     }
     
-    console.log("\n Outer Some Desc ", num);
+    console.log("Outer Some Desc ", num);
     
     return someOther
 }
